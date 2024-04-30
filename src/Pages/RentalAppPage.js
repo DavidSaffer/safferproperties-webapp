5df2965b049d4d5dc74860ff417558bd9cbe134d
davidsaffer <davidsaffer@vt.edu>
2024-04-30 14:38:57 -0400

    rental app page now matches other style

--- src/Pages/RentalAppPage.js
+++ src/Pages/RentalAppPage.js
@@ -1,53 +1,77 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import rentalApp from '../Assets/rentalApp.pdf'; // Import the PDF file
-import styles from './CSS/RentalApp.module.css'; // Import the CSS module
+import { motion } from 'framer-motion';
+import rentalApp from '../Assets/rentalAppForm.pdf';
+import styles from './CSS/RentalApp.module.css';
 
 function RentalApplication() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const address = queryParams.get('address');
-
-  // Construct the link address
-  //const linkAddress = address ? `properties/${encodeURIComponent(address.replace(/\s+/g, '-'))}` : '';
-
   const navigate = useNavigate();
 
+  const headerVariants = {
+    hidden: { opacity: 0, scale: 0.9 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        delay: 0.2,
+      },
+    },
+  };
+
+  const containerVariants = {
+    hidden: { opacity: 0, scale: 0.95 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>How to Apply</h1>
-      <p className={styles.instructions}>
-        To apply for{' '}
-        {address ? `the property located at ${address}` : 'the rental'}, please
-        follow the instructions below:
-      </p>
-      <ol>
-        <li>
-          Download the{' '}
-          {address
-            ? `application form for ${address}`
-            : 'rental application form'}{' '}
-          by clicking{' '}
-          <a className={styles.link} href={rentalApp} download>
-            here
-          </a>
-          .
-        </li>
-        <li>Fill out the application form with accurate information.</li>
-        <li>
-          Submit the completed application form to the landlord or property
-          manager.
-        </li>
-      </ol>
-      <p>
-        If you have any questions, please contact us at example@example.com.
-      </p>
-      {address && (
-        <button className={styles.returnButton} onClick={() => navigate(-1)}>
-          Return to {address}
-        </button>
-      )}
-    </div>
+    <motion.div className={styles.container} initial="hidden" animate="visible" variants={containerVariants}>
+      <motion.h1 className={styles.title} variants={headerVariants}>
+        How to Apply
+      </motion.h1>
+      <motion.div className={styles.textBlock}>
+        <motion.p className={styles.instructions} variants={itemVariants}>
+          To apply for {address ? `the property at ${address}` : 'a rental'}, please follow the instructions below:
+        </motion.p>
+        <motion.ol>
+          <motion.li variants={itemVariants}>
+            Download the {address ? `application form for ${address}` : 'rental application form'} by clicking{' '}
+            <a className={styles.link} href={rentalApp} download>
+              here
+            </a>
+            .
+          </motion.li>
+          <motion.li variants={itemVariants}>Fill out the application form with accurate information.</motion.li>
+          <motion.li variants={itemVariants}>Submit the completed form to the landlord or property manager.</motion.li>
+        </motion.ol>
+        {address && (
+          <motion.button className={styles.returnButton} onClick={() => navigate(-1)} variants={itemVariants}>
+            Return to {address}
+          </motion.button>
+        )}
+      </motion.div>
+    </motion.div>
   );
 }
 
